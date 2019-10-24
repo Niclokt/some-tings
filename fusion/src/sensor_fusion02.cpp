342b9fb8d49dfcacaf46e8107d12babeafae3123
Niclokt <e0200773@u.nus.edu>
2019-10-24 16:43:01 +0800

    feat:  included CallBack functions

--- fusion/src/sensor_fusion02.cpp
+++ fusion/src/sensor_fusion02.cpp
@@ -47,8 +47,8 @@ class SensorFusion
 
   // ROS Subscribers
   ros::Subscriber odom_sub;
-  ros::Subscriber obstacles_sub;
-  ros::Subscriber camera_sub;
+  ros::Subscriber obstacle_sub;
+  ros::Subscriber obstacle2_sub;
 
   // ROS Publishers
   ros::Publisher fused_obstacles_pub;
@@ -68,14 +68,28 @@ class SensorFusion
   void compareLidarWithCamera();
   void addCameraObstacleData(int i, int j);
   void lidarObstacleDataOnly(int i);
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg);
+  void obstacleCallback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg);
+  void obstacle2Callback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg);
 };
+
+
 
 //My Own Constructor
 SensorFusion::SensorFusion() : tf_listener(tf_buffer)
 {
   // Subscribe & Advertise
-	polarCoord_sub = nh.subscribe(polarCoord_topic_, 1, &SensorFusion::polarCoordCallback, this);
-  imgObAngle_sub = nh.subscribe(imgObAngle_topic_, 1, &SensorFusion::imgObAngleCallback, this);
+
+  // odom_topic 
+  // obstacle_topic (Lidar obs sub)
+  // detection_results (cam obs sub)
+  // publish to another topic
+
+
+	odom_sub = nh.subscribe(odom_topic_, 1, &SensorFusion::odomCallback, this);
+  obstacle_sub = nh.subscribe(obstacle_topic_, 1, &SensorFusion::obstacleCallback, this);
+  obstacle2_sub = nh.subscribe(obstacle2_topic_, 1, &SensorFusion::obstacle2Callbac
+  k, this);
 
 	//ROS_ASSERT (to assign constant values in launch file. No need to compile when changes are made to these values)
   ROS_ASSERT(private_nh.getParam("camera_fov", FOV_));
@@ -105,8 +119,7 @@ void SensorFusion::getPolarCoordinate(double ob_x, double ob_y)
 void SensorFusion::getImageObstacleAngle()
 {
   double alpha = deg2rad(FOV_ / 2);
-  double img_theta = atan(tan(alpha) * ((img_size[0]/ 2) - image_obstacle_x) / (img_size[0]/ 2));
-
+  double img_theta = atan(tan(alpha) * ((img_size[0]/ 2) - image_obstacle_x) / (img_size[0]/ 2)); //should it be img_size[1] instead?  
   camera_theta.push_back(img_theta);
   
   cout << "alpha: " << alpha << ", img_theta: " << img_theta << endl;
@@ -152,6 +165,69 @@ void SensorFusion::lidarObstacleDataOnly(int i)
 {
   fused_obstacles.push_back({obstacle_x[i], obstacle_y[i], lidar_theta[i], radius[i]}); // include lidar data only
   cout << fused_obstacles[i][0] << " " << fused_obstacles[i][1] << " " << fused_obstacles[i][2] << " " << fused_obstacles[i][3] << endl;
+}
+
+void SensorFusion::odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg)
+{
+	geometry_msgs::TransformStamped transform_stamped;
+	try
+	{
+	transform_stamped = tf_buffer.lookupTransform("map", "odom", ros::Time(0));
+	}
+	catch (tf2::TransformException& ex)
+	{
+	ROS_WARN("%s", ex.what());
+	return;
+	}
+
+	geometry_msgs::PoseStamped pose_before_transform, pose_after_transform;
+	pose_before_transform.header.frame_id = odom_msg->header.frame_id;
+	pose_before_transform.header.stamp = odom_msg->header.stamp;
+	pose_before_transform.pose = odom_msg->pose.pose;
+	tf2::doTransform(pose_before_transform, pose_after_transform, transform_stamped);
+
+	tf::Quaternion q(pose_after_transform.pose.orientation.x, pose_after_transform.pose.orientation.y,
+					pose_after_transform.pose.orientation.z, pose_after_transform.pose.orientation.w);
+	tf::Matrix3x3 m(q);
+	double roll, pitch;
+	m.getRPY(roll, pitch, car_yaw);
+
+	// Current XY of robot (map frame)
+	car_x = pose_after_transform.pose.position.x;
+	car_y = pose_after_transform.pose.position.y;
+}
+
+void SensorFusion::obstacleCallback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg)
+{
+	obstacles.clear();
+
+	for (int i =0; i < obstacle_msg->circles.size(); i++) 
+	{
+		double obstacle_x = obstacle_msg->circles[i].center.x;
+		double y = obstacle_msg->circles[i].center.y;
+		double r = obstacle_msg->circles[i].radius;
+		double vx = obstacle_msg->circles[i].velocity.x;
+		double vy = obstacle_msg->circles[i].velocity.y;
+		obstacles.push_back({x, y, r, vx, vy});
+	}
+	return;
+}
+
+//assuming this is camera data
+void SensorFusion::obstacle2Callback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg)
+{
+	// obstacles_2.clear();
+	
+	for (int i =0; i < obstacle_msg->circles.size(); i++) 
+	{
+		double image_obstacle_x = obstacle_msg->circles[i].center.x;
+		double y = obstacle_msg->circles[i].center.y;
+		double r = obstacle_msg->circles[i].radius;
+		double vx = obstacle_msg->circles[i].velocity.x;
+		double vy = obstacle_msg->circles[i].velocity.y;
+		obstacles_2.push_back({x, y, r, vx, vy});
+	}
+	return;
 }
 
 int main(int argc, char** argv)
