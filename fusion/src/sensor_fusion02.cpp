782fbe54b4c6872522e39a3909196d3442c73336
Niclokt <e0200773@u.nus.edu>
2019-10-25 17:12:28 +0800

    feat: included main callback function, scan_ obs_x

--- fusion/src/sensor_fusion02.cpp
+++ fusion/src/sensor_fusion02.cpp
@@ -1,10 +1,10 @@
-#include <ros/ros.h>
-#include <ros/console.h>
-#include <tf/tf.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/TransformStamped.h>
+// #include <ros/ros.h>
+// #include <ros/console.h>
+// #include <tf/tf.h>
+// #include <tf2_ros/transform_listener.h>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+// #include <nav_msgs/Odometry.h>
+// #include <geometry_msgs/TransformStamped.h>
 
 #include <iostream>
 #include <vector>
@@ -13,7 +13,14 @@
 
 #define BASELINK_TO_CAMERA 2.2 // in [m]
 
-using namespace std;
+using std::vector;
+using std::cout;
+using std::endl;
+
+// Functions (not methods)
+constexpr double pi() { return M_PI; }
+double deg2rad(double angle) { return angle * pi() / 180; }
+double rad2deg(double angle) { return angle / pi() * 180; }
 
 class SensorFusion
 {
@@ -30,6 +37,7 @@ class SensorFusion
 
  private:
   // Private Variables
+  double fusion_frequency_;
   
   const double FOV_ = 78; // in degrees
 
@@ -37,13 +45,18 @@ class SensorFusion
   double car_x;
   double car_y;
   double car_yaw;
-  double image_obstacle_x;
+
 
   const vector<double> img_size = {640, 480};
 
-  vector<double> obstacle_x, obstacle_y;
+
+  vector<vector<double>> obstacles; // from /obstacles topic -- 3D LiDAR
+	vector<vector<double>> obstacles_2; // from /obstacles2 topic -- 2D LiDAR
+  vector<double> image_obstacle_x;
+
+  vector<double> scan_obstacle_x, scan_obstacle_y; // extract x and y from /obstacles topic and /obstacles2 topic
   vector<double> lidar_theta, radius, camera_theta;
-  vector <vector<double> > fused_obstacles;
+  vector<vector<double>> fused_obstacles;
 
   // ROS Subscribers
   ros::Subscriber odom_sub;
@@ -57,23 +70,22 @@ class SensorFusion
 	tf2_ros::Buffer tf_buffer;
 	tf2_ros::TransformListener tf_listener;
 
-  // Private Functions
-  constexpr double pi();
-  double deg2rad(double angle);
-  double rad2deg(double angle);
+  //timer
+	ros::Timer timer;
 
-  // Private Methods
-  void getPolarCoordinate(double ob_x, double ob_y);
+  // Private Methods / Functions
+  void getPolarCoordinate();
   void getImageObstacleAngle();
   void compareLidarWithCamera();
   void addCameraObstacleData(int i, int j);
   void lidarObstacleDataOnly(int i);
+
   void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg);
   void obstacleCallback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg);
   void obstacle2Callback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg);
+
+  void mainTimerCallback();
 };
-
-
 
 //My Own Constructor
 SensorFusion::SensorFusion() : tf_listener(tf_buffer)
@@ -88,41 +100,54 @@ SensorFusion::SensorFusion() : tf_listener(tf_buffer)
 
 	odom_sub = nh.subscribe(odom_topic_, 1, &SensorFusion::odomCallback, this);
   obstacle_sub = nh.subscribe(obstacle_topic_, 1, &SensorFusion::obstacleCallback, this);
-  obstacle2_sub = nh.subscribe(obstacle2_topic_, 1, &SensorFusion::obstacle2Callbac
-  k, this);
-
+  obstacle2_sub = nh.subscribe(obstacle2_topic_, 1, &SensorFusion::obstacle2Callback, this);
 	//ROS_ASSERT (to assign constant values in launch file. No need to compile when changes are made to these values)
+  ROS_ASSERT(private_nh.getParam("fusion_frequency", fusion_frequency_));
   ROS_ASSERT(private_nh.getParam("camera_fov", FOV_));
   ROS_ASSERT(private_nh.getParam("tolerance_for_comparison", tolerance));
   ROS_ASSERT(private_nh.getParam("image_dimensions", img_size));
+
+  // Initialize the timer
+	timer = nh.createTimer(ros::Duration(1.0/fusion_frequency_), &AgvPlanner::mainTimerCallback, this);
 }
 
-constexpr double pi() { return M_PI; }
-double deg2rad(double angle) { return angle * pi() / 180; }
-double rad2deg(double angle) { return angle / pi() * 180; }
+void SensorFusion::mainTimerCallback()
+{
+  // TODO: Main Logic Here
+  ;
+}
 
-void SensorFusion::getPolarCoordinate(double ob_x, double ob_y)
+void SensorFusion::getPolarCoordinate()
 {
   // Compute lidar obstacles into camera polar coordinate
-  double camera_x = car_x + BASELINK_TO_CAMERA * cos(car_yaw);
-  double camera_y = car_y + BASELINK_TO_CAMERA * sin(car_yaw);
-  double r = sqrt(pow(ob_x - camera_x, 2) + pow(ob_y - camera_y, 2));
-  double scan_theta = atan2(ob_y - camera_y, ob_x - camera_x) - car_yaw;
+  int i=0;
+  for (i; i<scan_obstacle_x.size();i++)
+  {
+    double camera_x = car_x + BASELINK_TO_CAMERA * cos(car_yaw);
+    double camera_y = car_y + BASELINK_TO_CAMERA * sin(car_yaw);
+    double r = sqrt(pow(scan_obstacle_x[i] - camera_x, 2) + pow(scan_obstacle_y[i] - camera_y, 2));
+    double scan_theta = atan2(scan_obstacle_y[i] - camera_y, scan_obstacle_x[i] - camera_x) - car_yaw;
 
-  lidar_theta.push_back(scan_theta);
-  radius.push_back(r);
+    lidar_theta.push_back(scan_theta);
+    radius.push_back(r);
 
-  cout << "camera_x: " << camera_x << ", camera_y: " << camera_y 
-       << ", r: " << r << ", scan_theta: " << scan_theta << endl;
+    cout << "camera_x: " << camera_x << ", camera_y: " << camera_y 
+          << ", r: " << r << ", scan_theta: " << scan_theta << endl;
+  }
 }
 
 void SensorFusion::getImageObstacleAngle()
 {
-  double alpha = deg2rad(FOV_ / 2);
-  double img_theta = atan(tan(alpha) * ((img_size[0]/ 2) - image_obstacle_x) / (img_size[0]/ 2)); //should it be img_size[1] instead?  
-  camera_theta.push_back(img_theta);
-  
-  cout << "alpha: " << alpha << ", img_theta: " << img_theta << endl;
+  int i=0;
+  for (i; i<image_obstacle_x.size();i++)
+  {
+    double alpha = deg2rad(FOV_ / 2);
+    double img_theta = atan(tan(alpha) * ((img_size[0]/ 2) - image_obstacle_x[i]) / (img_size[0]/ 2));
+    camera_theta.push_back(img_theta);
+    
+    cout << "alpha: " << alpha << ", img_theta: " << img_theta << endl;
+  }
+
 }
 
 void SensorFusion::compareLidarWithCamera()
@@ -157,13 +182,13 @@ void SensorFusion::compareLidarWithCamera()
 
 void SensorFusion::addCameraObstacleData(int i, int j)
 {
-  fused_obstacles.push_back({obstacle_x[i], obstacle_y[i], lidar_theta[i], radius[i]}); // add camera data to lidar
+  fused_obstacles.push_back({scan_obstacle_x[i], scan_obstacle_y[i], lidar_theta[i], radius[i]}); // combine camera data to lidar
   cout << fused_obstacles[i][0] << " " << fused_obstacles[i][1] << " " << fused_obstacles[i][2] << " " << fused_obstacles[i][3] << endl;
 }
 
 void SensorFusion::lidarObstacleDataOnly(int i)
 {
-  fused_obstacles.push_back({obstacle_x[i], obstacle_y[i], lidar_theta[i], radius[i]}); // include lidar data only
+  fused_obstacles.push_back({scan_obstacle_x[i], scan_obstacle_y[i], lidar_theta[i], radius[i]}); // include lidar data only
   cout << fused_obstacles[i][0] << " " << fused_obstacles[i][1] << " " << fused_obstacles[i][2] << " " << fused_obstacles[i][3] << endl;
 }
 
@@ -203,12 +228,16 @@ void SensorFusion::obstacleCallback(const obstacle_detector::Obstacles::ConstPtr
 
 	for (int i =0; i < obstacle_msg->circles.size(); i++) 
 	{
-		double obstacle_x = obstacle_msg->circles[i].center.x;
+		double x = obstacle_msg->circles[i].center.x;
 		double y = obstacle_msg->circles[i].center.y;
 		double r = obstacle_msg->circles[i].radius;
 		double vx = obstacle_msg->circles[i].velocity.x;
 		double vy = obstacle_msg->circles[i].velocity.y;
-		obstacles.push_back({x, y, r, vx, vy});
+		
+    obstacles.push_back({x, y, r, vx, vy});
+
+    scan_obstacle_x = obstacles[0];
+    scan_obstacle_y = obstacles[1];
 	}
 	return;
 }
@@ -216,11 +245,11 @@ void SensorFusion::obstacleCallback(const obstacle_detector::Obstacles::ConstPtr
 //assuming this is camera data
 void SensorFusion::obstacle2Callback(const obstacle_detector::Obstacles::ConstPtr& obstacle_msg)
 {
-	// obstacles_2.clear();
+	 obstacles_2.clear();
 	
 	for (int i =0; i < obstacle_msg->circles.size(); i++) 
 	{
-		double image_obstacle_x = obstacle_msg->circles[i].center.x;
+		double x = obstacle_msg->circles[i].center.x;
 		double y = obstacle_msg->circles[i].center.y;
 		double r = obstacle_msg->circles[i].radius;
 		double vx = obstacle_msg->circles[i].velocity.x;
